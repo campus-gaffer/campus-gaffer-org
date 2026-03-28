bdf33992f3d92e1e0b65430720f6bb1e586a538d
ThePyLord <geraldfeu@gmail.com>
2026-03-28 10:59:27 -0500

    Refactored and updated models. Returned gorm.DB for dependency injection in db.go file.

--- campus-gaffer-backend/internal/models/game.go
+++ campus-gaffer-backend/internal/models/game.go
@@ -13,5 +13,7 @@ type Game struct {
 	Team1Id        uuid.UUID  `gorm:"type:uuid;not null"`
 	Team2Id        uuid.UUID  `gorm:"type:uuid;not null"`
 	KickoffTime    *time.Time `gorm:"type:timestamptz;"`
+	Status         string     `gorm:"type:string;not null;default:'scheduled'"`
 	IsScraped      bool       `gorm:"type:boolean;default:false;not null"`
+	UpdatedAt      time.Time  `gorm:"type:timestamp;not null"`
 }
--- campus-gaffer-backend/internal/models/player.go
+++ campus-gaffer-backend/internal/models/player.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 type Player struct {
 	Id               uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid();not null"`
-	ExternalPlayerId string    `gorm:"type:string;not null"`
+	ExternalId string    `gorm:"type:string;not null"`
 	ExternalSource   string    `gorm:"type:string;not null"`
 	Name             string    `gorm:"type:string;not null"`
 	BirthDate        *string    `gorm:"type:string;"`
