322b641ec01f2d75b9901b49fc5a189b652476e0
ThePyLord <geraldfeu@gmail.com>
2026-05-16 23:05:23 -0500

    Improved database connection function by enforcing singleton behaviour. Prevents the creation of multiple connections.

--- campus-gaffer-backend/internal/database/db.go
+++ campus-gaffer-backend/internal/database/db.go
@@ -4,13 +4,17 @@ import (
 	// "campus-gaffer-backend/internal/models"
 	"log"
 	"os"
-
+	"sync"
+	
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	initDB sync.Once
+	DB *gorm.DB
+)
 
 func LoadDBUri() string {
 	err := godotenv.Load()
@@ -21,11 +25,18 @@ func LoadDBUri() string {
 }
 
 func Connect(db_uri string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(db_uri), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to Neon")
-	}
-	log.Println("Connected to Neon")
+	initDB.Do(func() {
+		db, err := gorm.Open(postgres.Open(db_uri), &gorm.Config{})
+		if err != nil {
+			log.Fatal("Failed to connect to Neon")
+		}
+		log.Println("Connected to Neon")
+		DB = db
+		if DB == nil {
+			log.Fatal("Failed to set DB to db!")
+		}
+	})
+	// db, err := gorm.Open(postgres.Open(db_uri), &gorm.Config{})
 	// err = db.AutoMigrate(
 	// 	&models.Game{},
 	// 	&models.Player{},
@@ -38,6 +49,6 @@ func Connect(db_uri string) *gorm.DB {
 	// 	log.Fatal("Failed to automigrate")
 	// }
 	// log.Println("Successfully ran automigrate!")
-	DB = db
+	// DB = db
 	return DB
 }
